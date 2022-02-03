14ba6178c7d52ece5e05c93800b811874eb78ccd
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-02-03 09:51:12 +0530

    app

--- src/App.js
+++ src/App.js
@@ -26,4 +26,71 @@ function DistortedTorus() {
     () => [new THREE.Euler(), new THREE.Quaternion()],
     []
   );
+
+  useLayoutEffect(() => {
+    textures.forEach(
+      (texture) => (
+        (texture.wrapT = texture.wrapS = THREE.RepeatWrapping),
+        texture.repeat.set(4, 4)
+      )
+    );
+  }, [textures]);
+
+  useFrame(({ mouse }) => {
+    rEuler.set(
+      (mouse.y * viewport.height) / 300,
+      (mouse.x * viewport.width) / 300,
+      0
+    );
+    ref.current.quaternion.slerp(rQuaternion.setFromEuler(rEuler), 0.1);
+  });
+
+  return (
+    <>
+      <Torus ref={ref} args={[TORUS_RADIUS, 1.5, 128, 516]}>
+        <DistortTorusMaterial
+          color="#a19266"
+          metalness={1}
+          roughness={0.8}
+          radius={TORUS_RADIUS}
+          resolution={[size.width, size.height]}
+          aoMap={ao}
+          normalMap={normal}
+          normalScale={[2, 2]}
+          displacementMap={height}
+          roughnessMap={roughness}
+        />
+      </Torus>
+    </>
+  );
 }
+
+function App() {
+  return (
+    <>
+      <Canvas
+        pixelRatio={[1, 2]}
+        camera={{
+          position: [0, 0, 20],
+          near: 0.1,
+          far: 100,
+          fov: 50
+        }}
+      >
+        <spotLight intensity={2} position={[0, 30, 40]} />
+        <spotLight intensity={2} position={[-50, 30, 40]} />
+        <group position={[0, 0, isMobile ? -16 : 0]}>
+          <Suspense fallback={null}>
+            <Environment files="photo_studio_01_1k.hrd" />
+            <DistortedTorus />
+          </Suspense>
+        </group>
+      </Canvas>
+      <Loader />
+      <Badge />
+      <Overlay />
+    </>
+  );
+}
+
+export default App;
